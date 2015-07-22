b2617c285da4b0137bd703130b77668c5c321d86
Master2sid2 <master2sid2@i.ua>
2015-07-22 12:15:08 +0300

    update

---
+++ lesson5/Task1/src/AtachLine.java
@@ -0,0 +1,35 @@
+import com.sun.java.swing.plaf.windows.WindowsInternalFrameTitlePane;
+import com.sun.javaws.exceptions.ExitException;
+
+import javax.security.auth.DestroyFailedException;
+import java.util.Scanner;
+
+/**
+ * Created by Master on 7.22.2015.
+ */
+public class AtachLine {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Введите строку:");
+        String s = scn.nextLine();
+        System.out.println("Введите целое число:");
+
+        int y=0; float z=0;
+
+        try { y = scn.nextInt();
+        } catch (Exception e){
+            System.out.println("Вы ввели строку");
+        }
+        System.out.println("Введите число с плавающей точкой:");
+
+        try { z = scn.nextFloat();
+        } catch (Exception e){
+            System.out.println("Вы ввели строку");
+        }
+        myAtach(s,y,z);
+    }
+
+    static void myAtach(String x,int y, float z){
+        System.out.println("S = " + x + "+" + y + "+" + z);
+    }
+}
---
+++ lesson5/Task2/src/ChangeArray.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Created by Master on 7.22.2015.
+ */
+public class ChangeArray {
+    public static void main(String[] args) {
+
+        Random rnd = new Random();
+        int rndTmp = 5 + (int) (Math.random() * ((10 - 5) + 1));
+
+        int[] arr = new int[rndTmp];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rnd.nextInt(10) + 1;
+        }
+        System.out.println("Созданный массив:");
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Увелечение первых трёх элементов на 1:");
+        tryChange(arr);
+    }
+
+    static void tryChange(int[] array) {
+
+        int sum = 0;
+        for (int i = 0; i < 3; i++) {
+            array[i] = array[i] + 1;
+            sum += array[i];
+        }
+        System.out.println(Arrays.toString(array));
+        System.out.println("Сумма первых трёх элементов массива = " + sum);
+    }
+}
