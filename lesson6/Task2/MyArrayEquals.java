4066be3f4af2457d30bd28a51857fab9d554afdb
Master2sid2 <master2sid2@i.ua>
2015-07-23 02:23:49 +0300

    update

--- lesson6/Task2/MyArrayEquals.java
+++ lesson6/Task2/MyArrayEquals.java
@@ -7,15 +7,18 @@ public class MyArrayEquals {
 
         short[] arraySource = {15,-56,555,-1347,234};
         short[] arrayTarget = {15,-56,555,-1347,234};
+        short[] arrayTargetNew = {15,-56,555,-1347};
 
         System.out.println(myEquals(arraySource,arrayTarget));
+        System.out.println(myEquals(arrayTarget,arrayTargetNew));
     }
 
     static boolean myEquals(short[] arraySource,short[] arrayTarget){
-        boolean bool = true;
+        if(arraySource.length!=arrayTarget.length)
+            return false;
         for (int i=0; i<arraySource.length; i++)
             if (arraySource[i] != arrayTarget[i])
                 return false;
-        return bool;
+        return true;
     }
 }
