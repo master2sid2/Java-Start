d6bf714c5b6d2eb0215ea0cb1b0da29748794182
Master2sid2 <master2sid2@i.ua>
2015-08-07 17:49:16 +0300

    update

---
+++ OOP/Present/Task2/StaticCount.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Master on 8.7.2015.
+ */
+public class StaticCount {
+
+    private static int count = 0;
+
+    public StaticCount(){
+        count++;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+}
