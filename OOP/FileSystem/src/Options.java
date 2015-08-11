d3c044591bdd94837fb9f7350fa2f80782f55693
Master2sid2 <master2sid2@i.ua>
2015-08-11 19:24:29 +0300

    update

--- OOP/FileSystem/src/Options.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Master on 8.11.2015.
- */
-public interface Options {
-    String name1(String name);
-    int size(int size);
-    String type(String type);
-    boolean mark(boolean mark);
-}
